e5aa6532b91e3fc63b9128b38f3f5f00daafd98b
Tonya <elzeresdai@gmail.com>
2023-02-07 14:25:56 +0200

    docker, migration

--- database/migrations/2023_02_05_185142_create_data_csvs_table.php
+++ database/migrations/2023_02_05_185142_create_data_csvs_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('data_csv', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->integer('start_range');
+            $table->integer('end_range');
+            $table->integer('length');
+            $table->integer('description');
+            $table->integer('record_type');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('data_csv');
     }
 };
